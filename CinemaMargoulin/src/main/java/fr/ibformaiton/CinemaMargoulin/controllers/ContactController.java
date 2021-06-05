b03fb2908c2bb38f3fbeb1a3f228a3e490c8a0f7
DESKTOP-5L7N2A8\ib <thualanthony@gmail.com>
2021-06-05 18:14:36 +0200

    deleteById

--- CinemaMargoulin/src/main/java/fr/ibformaiton/CinemaMargoulin/controllers/ContactController.java
+++ CinemaMargoulin/src/main/java/fr/ibformaiton/CinemaMargoulin/controllers/ContactController.java
@@ -7,9 +7,11 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.ibformaiton.CinemaMargoulin.Services.ContactService;
@@ -40,6 +42,13 @@ public class ContactController {
 	    @Transactional // A ajouter lorsqu'on modifie qqc en BDD
 	    public void addContact(@RequestBody Contact contact) {
 		 contactService.add(contact);
+
+	    }
+	 
+	 @DeleteMapping("/REST/deleteContactById")
+	    @Transactional // A ajouter lorsqu'on modifie qqc en BDD
+	    public void deleteContactById(@RequestParam("id") int id) {
+	        contactService.deleteContactById(id);
 
 	    }
 }
