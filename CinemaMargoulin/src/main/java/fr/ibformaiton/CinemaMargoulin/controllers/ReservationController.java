1cd5259bb028d36a7b63e1e0c04833bc5ef79c1f
DESKTOP-5L7N2A8\ib <branjonyoann@hotmail.fr>
2021-06-06 20:20:39 +0200

    Ajout de l'update dans les deux controllers

--- CinemaMargoulin/src/main/java/fr/ibformaiton/CinemaMargoulin/controllers/ReservationController.java
+++ CinemaMargoulin/src/main/java/fr/ibformaiton/CinemaMargoulin/controllers/ReservationController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.ibformaiton.CinemaMargoulin.Services.ReservationService;
+import fr.ibformaiton.CinemaMargoulin.beans.Film;
 import fr.ibformaiton.CinemaMargoulin.beans.Reservation;
 import fr.ibformaiton.CinemaMargoulin.beans.Users;
 
@@ -47,6 +49,13 @@ public class ReservationController {
 	    @Transactional // A ajouter lorsqu'on modifie qqc en BDD
 	    public void deleteReservationById(@RequestParam("id") int id) {
 	        reservationService.deleteReservationById(id);
+
+	    }
+	 
+	 @PutMapping("/REST/updateReservation")
+	    @Transactional // A ajouter lorsqu'on modifie qqc en BDD
+	    public void updateReservation(@RequestBody Reservation reservation) {
+	        reservationService.update(reservation);
 
 	    }
 	 
--- CinemaMargoulin/src/main/java/fr/ibformaiton/CinemaMargoulin/controllers/SessionController.java
+++ CinemaMargoulin/src/main/java/fr/ibformaiton/CinemaMargoulin/controllers/SessionController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.ibformaiton.CinemaMargoulin.Services.SessionService;
+import fr.ibformaiton.CinemaMargoulin.beans.Film;
 import fr.ibformaiton.CinemaMargoulin.beans.Session;
 import fr.ibformaiton.CinemaMargoulin.beans.Users;
 
@@ -47,6 +49,13 @@ public class SessionController {
 	    @Transactional // A ajouter lorsqu'on modifie qqc en BDD
 	    public void deleteSessionById(@RequestParam("id") int id) {
 	        sessionService.deleteSessionById(id);
+
+	    }
+	 
+	 @PutMapping("/REST/updateSession")
+	    @Transactional // A ajouter lorsqu'on modifie qqc en BDD
+	    public void updateFilm(@RequestBody Session session) {
+	        sessionService.update(session);
 
 	    }
 	  
